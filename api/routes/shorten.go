ab9ae1112f3a2530707e665958d6f4708d8c0d83
RyanDerr <JamesRDerr@gmail.com>
2024-09-03 14:30:19 -0500

    Add Rate Limiting

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,10 +1,15 @@
 package routes
 
 import (
+	"os"
+	"ryan-golang-url-shortener/database"
 	"ryan-golang-url-shortener/helpers"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -28,15 +33,75 @@ func ShortenURL(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
 	}
 
-	//Rate limits
+	//Rate limit with redis in memory database based on IP address
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	if err := handleRateLimiting(c, r2); err != nil {
+		return err
+	}
 
 	if !govalidator.IsURL(body.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
 	}
 
 	if !helpers.RemoveDomainError(body.URL) {
-		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "You do not have permission to shorten this URL"})
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "You cannot shorten this URL"})
 	}
 
 	body.URL = helpers.EnforceHTTPS(body.URL)
+
+	var id string
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ := r.Get(database.Ctx, id).Result()
+
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "URL short is already in use."})
+	}
+
+	if body.Expiration == 0 {
+		body.Expiration = 24 * time.Hour
+	}
+
+	setDBError := r.Set(database.Ctx, id, body.URL, body.Expiration).Err()
+
+	if setDBError != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal Server Error"})
+	}
+
+	//Decrement rate limit
+	r2.Decr(database.Ctx, c.IP())
+}
+
+func handleRateLimiting(c *fiber.Ctx, rc *redis.Client) error {
+	val, err := rc.Get(database.Ctx, c.IP()).Result()
+
+	//Check to see if the IP address is in the database
+	if err == redis.Nil {
+		rc.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*time.Minute).Err()
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal Server Error"})
+	} else {
+		// Find the current rate limit and determine if limit is exceeded
+		valInt, _ := strconv.Atoi(val)
+
+		if valInt <= 0 {
+			limit, _ := rc.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded!",
+				"rate_limit_reset": limit.Minutes(),
+			})
+		}
+	}
+
+	return nil
 }
