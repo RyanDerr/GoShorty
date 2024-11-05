6badb7ea3d353f5f7f67a6818e25a15a9564c967
RyanDerr <JamesRDerr@gmail.com>
2024-11-05 15:48:37 -0600

    Add some redis updates

--- api/database/database.go
+++ api/database/database.go
@@ -3,22 +3,13 @@ package database
 import (
 	"os"
 
-	"github.com/RyanDerr/GoShorty/api/config"
 	"github.com/redis/go-redis/v9"
 )
 
 func CreateRedisClient(dbNo int) *redis.Client {
-	var rdb *redis.Client
-	if config.IsLocal() {
-		rdb = redis.NewClient(&redis.Options{
-			Addr:     os.Getenv("REDIS_ADDRESS"),
-			Password: os.Getenv("DB_PASSWORD"),
-			DB:       dbNo,
-		})
-	} else {
-		// TODO: Implement Elasticache connection
-		rdb = nil
-	}
-
-	return rdb
+	return redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       dbNo,
+	})
 }
